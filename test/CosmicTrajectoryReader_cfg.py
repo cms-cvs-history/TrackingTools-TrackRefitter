3c3ad3296a013b7377bf643349dc5d1a767fbbf1
cmsbuild <>
2008-10-22 21:32:53 +0000

    This commit was manufactured by cvs2git to create tag 'TrackingTools- TrackRefitter-U00-03-01'.
    Sprout from master 2008-08-31 15:18:43 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Bugifx for uninitialized tracker recthis (HEAD version)'
    Cherrypick from master 2008-10-22 21:32:52 UTC Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch> 'TrackReader':
        test/BuildFile
        test/CosmicTrajectoryReader_cfg.py
        test/TrackReader.cc
        test/TrackReader.h
        test/TrackReader_cfg.py
        test/TrajectoryReader.cc
        test/TrajectoryReader_cfg.py

--- test/CosmicTrajectoryReader_cfg.py
+++ test/CosmicTrajectoryReader_cfg.py
@@ -1,19 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TEST")
+process = cms.Process("TESTFIT")
 
 
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
-process.load("TrackingTools.TrackRefitter.globalMuonTrajectories_cff")
-process.load("TrackingTools.TrackRefitter.standAloneMuonTrajectories_cff")
-process.load("TrackingTools.TrackRefitter.ctfWithMaterialTrajectories_cff")
+process.load("TrackingTools.TrackRefitter.globalCosmicMuonTrajectories_cff")
+process.load("TrackingTools.TrackRefitter.cosmicMuonTrajectories_cff")
+process.load("TrackingTools.TrackRefitter.ctfWithMaterialTrajectoriesP5_cff")
 
 process.load("Configuration.StandardSequences.Services_cff")
 
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'IDEAL_V6::All'
+process.GlobalTag.globaltag = 'CRZT210_V1::All' 
+process.prefer("GlobalTag")
 
 
 #process.load("Configuration.StandardSequences.Reconstruction_cff")
@@ -21,8 +25,11 @@ process.GlobalTag.globaltag = 'IDEAL_V6::All'
 process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEGeneric_cfi")
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_9/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v2/0000/2A00EECC-A185-DD11-93A9-000423D9517C.root')
-)
+                            fileNames = cms.untracked.vstring(
+    '/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/005F51E5-0373-DD11-B6FA-001731AF6B7D.root',
+    '/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/006F3A6A-0373-DD11-A8E7-00304876A0FF.root',
+    '/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/02CF5B1E-6476-DD11-A034-003048769E65.root'
+    ))
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
@@ -63,13 +70,13 @@ process.MessageLogger = cms.Service("MessageLogger",
 # )
 
 process.CTFTrajectoriesReader = cms.EDProducer("TrajectoryReader",
-                                             rootFileName = cms.untracked.string('CTFTajectoriesReader.root'),
-                                             InputLabel = cms.InputTag("generalTracks","Refitted")
-                                             )
+                                               rootFileName = cms.untracked.string('CTFTajectoriesReader.root'),
+                                               InputLabel = cms.InputTag("ctfWithMaterialTracksP5","Refitted")
+                                               )
 
 #process.glbMuons = cms.Sequence(process.globalMuons*process.GLBTrajectoriesReader)
 #process.staMuons = cms.Sequence(process.standAloneMuons*process.STATrajectoriesReader)
-process.tk = cms.Sequence(process.generalTracks*process.CTFTrajectoriesReader)
+process.tk = cms.Sequence(process.ctfWithMaterialTracksP5*process.CTFTrajectoriesReader)
 
 #process.testSTA = cms.Path(process.staMuons+process.tk+process.glbMuons)
 #process.testSTA = cms.Path(process.staMuons+process.tk)
